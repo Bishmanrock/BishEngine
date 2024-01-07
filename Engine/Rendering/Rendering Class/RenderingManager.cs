8bcffe4473d0bed54cfbce3eb2d408a8bb3b1e87
Bishmanrock <bishmanrock@outlook.com>
2024-01-07 14:41:05 +0000

    Sync before playing with shaders

--- Engine/Rendering/Rendering Class/RenderingManager.cs
+++ Engine/Rendering/Rendering Class/RenderingManager.cs
@@ -3,6 +3,7 @@
 // The Rendering Manager is a static class which is called every frame as part of the game loop. When objects are created they are added to the Rendering Manager, and when they are destroyed/disabled/etc. they are removed. The Rendering Manager cycles through every object in its list to draw them per frame. This works on a small scale, but may need to be expanded on if it becomes too cumbersome if projects ever get large enough. It's possible this may be a sufficient solution for the size games I make though.
 
 using JetBrains.Annotations;
+using OpenGL;
 using System.Runtime.InteropServices;
 
 namespace Engine
